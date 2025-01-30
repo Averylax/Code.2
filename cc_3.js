22b2478c1fb273d1289ed3dd6cf3d0170d3f4fdd
avery.white <averydouglascwhite@usf.edu>
2025-01-30 10:19:19 -0500

    Task 1 - Product Price Management Scenario

--- cc_3.js
+++ cc_3.js
@@ -1,2 +1,11 @@
 console.log(`cc_3.js is succesfully uploaded!`);
 
+// Task 1 - Product Price Manegment Scenario 
+let prices = [15, 30, 45, 60, 75]; // Prices of the products
+console.log(`Initial Prices: #${prices}`); // Logs the initial prices
+
+prices.push(90); // Adds 90 to the end of the array
+prices.shift(); // Removes the first element of the array
+
+console.log(`Updated Prices: $${prices}`); // Logs the updated prices
+// Task 1 - End
